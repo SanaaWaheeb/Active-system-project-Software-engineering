5801d85a95d70811d6a2c3c1eefc0452ca77604d
sanaa <Smahmoudwaheeb@stu.kau.edu.sa>
2022-12-15 23:25:49 +0300

    starting poject 251 group one

---
+++ src/pkg251_project_group_one/Main.java
@@ -0,0 +1,19 @@
+/*
+251 project
+ */
+package pkg251_project_group_one;
+
+/**
+ *
+ * @author DELL
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("project 251");
+    }
+    
+}
