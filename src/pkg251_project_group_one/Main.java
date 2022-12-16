dacbe3d0d8b8fc7d96f7f0bc552d99b43e2a34a4
taghreed <t-hammad@hotmail.com>
2022-12-16 23:20:50 +0300

    hate251

--- src/pkg251_project_group_one/Main.java
+++ src/pkg251_project_group_one/Main.java
@@ -14,6 +14,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("project 251");
+        System.out.println("taghreed hammad i hate 251");
     }
     
 }
