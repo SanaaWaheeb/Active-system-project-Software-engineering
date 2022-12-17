8776f1e7f851982a9246a350aba81f5bbc109930
sanaa <Smahmoudwaheeb@stu.kau.edu.sa>
2022-12-17 21:35:45 +0300

    starting poject 251 group one

--- src/pkg251_project_group_one/Main.java
+++ src/pkg251_project_group_one/Main.java
@@ -13,8 +13,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("project 251");
-        System.out.println("taghreed hammad i hate 251");
+        System.out.println("Sanaa of course we will have fun with project 251");
+        System.out.println("taghreed hammad i love 251");
         System.out.println("waad firs lab is done :) ");
         System.out.println("Esra = the coolest one ");
         System.out.println("Ftoon 'do u know who is Hamtaro?' ");
