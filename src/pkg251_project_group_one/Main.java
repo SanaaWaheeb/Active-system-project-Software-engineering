e23e8f8124487993e07f0c6ccdca459799104247
waad_alhashdi <HP@192.168.1.108>
2023-01-28 23:40:29 +0300

    Add if condition for checking  user name and password Make user enter event information  then add event to the system

--- src/pkg251_project_group_one/Main.java
+++ src/pkg251_project_group_one/Main.java
@@ -33,9 +33,71 @@ public class Main {
             String username = input.next();
             System.out.print("Enter Password: "); // leader password
             String password = input.next();
-
+// check if name and password is true 
+            if (username.equals(leaderOfSocialCommittees.getUserName()) && (password.endsWith(leaderOfSocialCommittees.getPassword())) || 
+                username.equals(leaderOfIslamicCommittees.getUserName()) && (password.endsWith(leaderOfIslamicCommittees.getPassword()))||
+                username.equals(leaderOfCulturalCommittees.getUserName()) && (password.endsWith(leaderOfCulturalCommittees.getPassword()))||
+                username.equals(leaderOfSportCommittees.getUserName()) && (password.endsWith(leaderOfSportCommittees.getPassword()))) {
+                int leaderChoice = 0;
+                while (leaderChoice != 5) {
+                   // show services of  the leader 
+        System.out.println("*****************************************************************************************************************");
+        System.out.println("                                          FCIT Activities system                                                 ");
+        System.out.println("*****************************************************************************************************************");
+        System.out.println("1. Add Event to System");
+        System.out.println("2. Delete Event From System");
+        System.out.println("3. Show Events In System");
+        System.out.println("4. Search For Event");
+        System.out.println("5. Logout");
+        System.out.println("******************************************************************************************************************");
+        System.out.print("Enter '1' to Add Event, '2' to Delete Event, '3'  Show Events, '4' Search For Event, '5' to Exit from System:  ");
+        
+                    leaderChoice = input.nextInt();
+                    switch (leaderChoice) {
+                        
+                        case 1:
+                        //add event
+                           System.out.print("Enter Event Name: ");
+                          String eventName = input.next();
+                            System.out.print("Enter Date of Event in format 'DD:MM:YY': ");
+                            String dateOfEvent = input.next();
+                           System.out.print("Enter Number Of Seats: ");
+                            int numberOfSeats = input.nextInt();
+                          System.out.print("Enter Description Of Event: ");
+                            String descriptionOfEvent = input.next();
+                            Event eventAdded = new Event(eventName,dateOfEvent,numberOfSeats,descriptionOfEvent);
+                            // add event to system 
+                            System.out.println(Event.addNewEvent(eventAdded));
+                            break;
+                        
+                        case 2:
+                        //delete event
+                          
+                      
+                        case 3:
+                        //show event
+                           
+                         
+                        case 4:
+                        //Search for Event
+                      
+                            default:
+                            break;}}
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
         } else {  //incorrect username or password
             System.out.println("Your UserName or Password are wrong, Try Again");
         }
-    }
+      }
+        
+    } 
+    
 }
