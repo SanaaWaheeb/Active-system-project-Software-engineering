aaf3b64f555cd09622be0c3883a194c4f493f3c4
sanaa <Smahmoudwaheeb@stu.kau.edu.sa>
2023-01-31 22:37:00 +0300

    review code

--- src/pkg251_project_group_one/Main.java
+++ src/pkg251_project_group_one/Main.java
@@ -9,20 +9,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Create new leaders for the system
+        //Create new object for leaders for the system
         Leader leaderOfSocialCommittees = new Leader("hanaa", "251_beautiful", "dr. Hanaa", "Ahmed", "Alamodi", "hanaa@stu.kau.edu.sa");
         Leader leaderOfIslamicCommittees = new Leader("fatama", "251_great", "dr. Fatama", "Mohammed", "Alrarhi", "Fatama@stu.kau.edu.sa");
         Leader leaderOfCulturalCommittees = new Leader("doaa", "251_fun", "dr. Doaa", "Tamer", "Alhashdi", "doaa@stu.kau.edu.sa");
         Leader leaderOfSportCommittees = new Leader("suaad", "251_nice", "dr. Suaad", "Ahmed", "Alshmrani", "suaad@stu.kau.edu.sa");
 
         //Print user interface on the screan 
-        System.out.println("*****************************************************************************************************************");
-        System.out.println("                                           FCIT Activities system                                         ");
-        System.out.println("*****************************************************************************************************************");
-        System.out.println(" 1. Leader");
-        System.out.println(" 2. Student");
-        System.out.println(" 3. Administrator");
-        System.out.println("******************************************************************************************************************");
+        System.out.println("********************************************************************************************************************");
+        System.out.println("                                              FCIT Activities system                                                ");
+        System.out.println("********************************************************************************************************************");
+        System.out.println(" 1. Leader..........................................................................................................");
+        System.out.println(" 2. Student.........................................................................................................");
+        System.out.println(" 3. Administrator...................................................................................................");
+        System.out.println("********************************************************************************************************************");
         System.out.print("Enter '1' if you are Leader, '2' if you are Student, '3'  if you are Administrator: ");
 
         //Create scanner and Read user choice
@@ -33,7 +33,7 @@ public class Main {
             String username = input.next();
             System.out.print("Enter Password: "); // leader password
             String password = input.next();
-// check if name and password is true 
+            // check if name and password are true 
             if (username.equals(leaderOfSocialCommittees.getUserName()) && (password.endsWith(leaderOfSocialCommittees.getPassword())) || 
                 username.equals(leaderOfIslamicCommittees.getUserName()) && (password.endsWith(leaderOfIslamicCommittees.getPassword()))||
                 username.equals(leaderOfCulturalCommittees.getUserName()) && (password.endsWith(leaderOfCulturalCommittees.getPassword()))||
@@ -43,13 +43,13 @@ public class Main {
                    // show services of  the leader 
         System.out.println("*****************************************************************************************************************");
         System.out.println("                                          FCIT Activities system                                                 ");
-        System.out.println("*****************************************************************************************************************");
-        System.out.println("1. Add Event to System");
-        System.out.println("2. Delete Event From System");
-        System.out.println("3. Show Events In System");
-        System.out.println("4. Search For Event");
-        System.out.println("5. Logout");
         System.out.println("******************************************************************************************************************");
+        System.out.println("1. Add Event to System............................................................................................");
+        System.out.println("2. Delete Event From System........................................................................................");
+        System.out.println("3. Show Events In System...........................................................................................");
+        System.out.println("4. Search For Event.................................................................................................");
+        System.out.println("5. Logout...........................................................................................................");
+        System.out.println("********************************************************************************************************************");
         System.out.print("Enter '1' to Add Event, '2' to Delete Event, '3'  Show Events, '4' Search For Event, '5' to Exit from System:  ");
         
                     leaderChoice = input.nextInt();
@@ -57,13 +57,13 @@ public class Main {
                         
                         case 1:
                         //add event
-                           System.out.print("Enter Event Name: ");
-                          String eventName = input.next();
+                            System.out.print("Enter Event Name: ");
+                            String eventName = input.next();
                             System.out.print("Enter Date of Event in format 'DD:MM:YY': ");
                             String dateOfEvent = input.next();
-                           System.out.print("Enter Number Of Seats: ");
+                            System.out.print("Enter Number Of Seats: ");
                             int numberOfSeats = input.nextInt();
-                          System.out.print("Enter Description Of Event: ");
+                            System.out.print("Enter Description Of Event: ");
                             String descriptionOfEvent = input.next();
                             Event eventAdded = new Event(eventName,dateOfEvent,numberOfSeats,descriptionOfEvent);
                             // add event to system 
@@ -72,21 +72,18 @@ public class Main {
                         
                         case 2:
                         //we call method delete event
-                      
                             System.out.print("Enter Event Name to delete it: ");
                             System.out.println(Event.deleteEvent(input.next()));
                             break;
-                      
                         case 3:
                         //we call method show event
                            Event.showEvent();
                             break;
-                         
                         case 4:
                         //Search for Event
                             System.out.print("Enter Event name: ");
                             String nameOfEvent = input.next();
-                              //search about event using name of event
+                            //search about event using name of event
                             Event eventsearched = Event.searchEvent(nameOfEvent);
                             //if the event dose not exit 
                             if (eventsearched == null) {
@@ -95,26 +92,15 @@ public class Main {
                             // print the information about existing Events in the system
                             else 
                             {System.out.println("*************************************************************************************************************");
-                             System.out.println("                                          FCIT Activities system                                          ");
+                             System.out.println("                                          FCIT Activities system                                             ");
                              System.out.println("*************************************************************************************************************");
-                                System.out.println( "The Name Of Event is: " + eventsearched.getEventName() + ", Number of seats  are: "
+                             System.out.println( "The Name Of Event is: " + eventsearched.getEventName() + ", Number of seats  are: "
                                          + eventsearched.getNumberOfSeats() +", The Event Date is: " + eventsearched.getDate() 
                                          + ", The Description Of Event is: "+eventsearched.getDescriptionOfEvent());
-                                System.out.println("****************************************************************************************************************");}
+                             System.out.println("**************************************************************************************************************");}
                             break;
-
                             default:
-                            break;}}
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+                            break;}} 
         } else {  //incorrect username or password
             System.out.println("Your UserName or Password are wrong, Try Again");
         }
